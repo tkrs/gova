8ba3716b6c7cad5b313e5695658d7d45a5ea6999
Takeru Sato <midium.size@gmail.com>
2014-09-20 20:04:23 +0900

    mod

--- jvmgo.go
+++ jvmgo.go
@@ -11,9 +11,6 @@ package main
 JNIEnv *env;
 JavaVM *jvm;
 
-int CallDestroyJavaVM();
-int InitJVM();
-
 int InitJVM() {
     int ret;
     JavaVMInitArgs vm_args;
@@ -21,14 +18,14 @@ int InitJVM() {
     JNI_GetDefaultJavaVMInitArgs(&vm_args);
     if (ret != JNI_OK) {
         printf("vm init args error:%d\n", ret);
-        return ret;
+        return -1;
     }
 
     JavaVMOption options[2];
     options[0].optionString = "-Djava.compiler=NONE";
-    //options[1].optionString = "-Djava.library.path=C:\\Program Files\\Java\\jdk1.8.0_05\\lib";
     options[1].optionString = "-Djava.class.path=.";
     //options[2].optionString = "-verbose:jni";
+    //options[3].optionString = "-Djava.library.path=C:\\Program Files\\Java\\jdk1.8.0_05\\lib";
 
     vm_args.nOptions = 1;
     vm_args.options = options;
@@ -36,65 +33,141 @@ int InitJVM() {
     ret = JNI_CreateJavaVM(&jvm, (void **)&env, &vm_args);
     if (ret != JNI_OK) {
         printf("create vm error:%d\n", ret);
-        return ret;
-    }
-
-}
-
-int Hello(char *name) {
-    jstring jname = (*env)->NewStringUTF(env, name);
-
-    jclass cls = (*env)->FindClass(env, "Hello");
-    if (cls == 0) {
-        fprintf(stderr, "FindClass error\n");
         return -1;
     }
-    jmethodID mid = (*env)->GetStaticMethodID(env, cls, "say", "(Ljava/lang/String;)V");
-     if (mid == 0) {
-        fprintf(stderr, "GetStaticMethodID error\n");
-        return -1;
-    }
-    (*env)->CallStaticVoidMethod(env, cls, mid, jname);
     return 0;
 }
 
-int CallDestroyJavaVM() {
+jstring _NewStringUTF(char *bytes) {
+    jstring ret = (*env)->NewStringUTF(env, bytes);
+    if (ret == 0) {
+        fprintf(stderr, "NewStringUTF error\n");
+    }
+    return ret;
+}
+
+jclass _FindClass(char *bytes) {
+    jclass ret = (*env)->FindClass(env, bytes);
+    if (ret == 0) {
+        fprintf(stderr, "NewStringUTF error\n");
+    }
+    return ret;
+}
+
+jmethodID _GetStaticMethodID(jclass clazz, const char *name, const char *sig) {
+    jmethodID ret = (*env)->GetStaticMethodID(env, clazz, name, sig);
+    if (ret == 0) {
+        fprintf(stderr, "GetStaticMethodID error\n");
+        return 0;
+    }
+    return ret;
+}
+
+void _CallStaticVoidMethod(jclass clazz, jmethodID methodID, jstring arg) {
+    (*env)->CallStaticVoidMethod(env, clazz, methodID, arg);
+}
+
+const char *_GetStringUTFChars(jstring string, jboolean *isCopy) {
+    const char *nativeString = (*env)->GetStringUTFChars(env, string, isCopy);
+    return nativeString;
+}
+
+int _ReleaseStringUTFChars(jstring string, const char *utf) {
+    (*env)->ReleaseStringUTFChars(env, string, utf);
+    return 0;
+}
+
+int DestroyJavaVM() {
     (*jvm)->DestroyJavaVM(jvm);
     return 0;
 }
 */
 import "C"
+import "log"
 
-type JNI struct{}
+// JNIEnv is java native interface
+type JNIEnv struct{}
 
-func NewJavaVM() *JNI {
+// NewJNIEnv returns JNIEnv pointer
+func NewJNIEnv() *JNIEnv {
 	ret, err := C.InitJVM()
-	if err != nil {
-		return nil
-	}
-	println(ret)
-	return new(JNI)
-}
 
-func (*JNI) Hello(name string) {
-	ret, err := C.Hello(C.CString(name))
 	if err != nil {
-		println(err)
 		panic(err)
 	}
-	println(ret)
+
+	if ret < 0 {
+		return nil
+	}
+
+	return &JNIEnv{}
 }
 
-func (j *JNI) DestroyJavaVM() {
-	ret, err := C.CallDestroyJavaVM()
+// NewStringUTF java native interface
+func (*JNIEnv) NewStringUTF(s string) (jstring C.jstring, err error) {
+	jstring, err = C._NewStringUTF(C.CString(s))
+	return
+}
+
+// FindClass java native interface
+func (*JNIEnv) FindClass(className string) (jclass C.jclass, err error) {
+	jclass, err = C._FindClass(C.CString(className))
+	return
+}
+
+// GetStaticMethodID java native interface
+func (*JNIEnv) GetStaticMethodID(clazz C.jclass, name string, sig string) (jmethodID C.jmethodID, err error) {
+	jmethodID, err = C._GetStaticMethodID(clazz, C.CString(name), C.CString(sig))
+	return
+}
+
+// CallStaticVoidMethod java native interface
+func (*JNIEnv) CallStaticVoidMethod(jclass C.jclass, jmethodID C.jmethodID, arg C.jstring) {
+	C._CallStaticVoidMethod(jclass, jmethodID, arg)
+}
+
+// GetStringUTFChars
+func (*JNIEnv) GetStringUTFChars(jstring C.jstring, isCopy *C.jboolean) (ret *C.char, err error) {
+	ret, err = C._GetStringUTFChars(jstring, isCopy)
+	return
+}
+
+func (*JNIEnv) ReleaseStringUTFChars(jstring C.jstring, utf *C.char) (err error) {
+	_, err = C._ReleaseStringUTFChars(jstring, utf)
+	return
+}
+
+// DestroyJavaVM is call destroy JavaVM
+func DestroyJavaVM() {
+	_, err := C.DestroyJavaVM()
 	if err != nil {
-		return
+		panic(err)
 	}
-	println(ret)
+	return
 }
 
 func main() {
-	jvm := NewJavaVM()
-	defer jvm.DestroyJavaVM()
-	jvm.Hello("桃白白")
+	env := NewJNIEnv()
+	if env == nil {
+		return
+	}
+	defer DestroyJavaVM()
+
+	arg, err := env.NewStringUTF("桃白白")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jclass, err := env.FindClass("Hello")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jmethodID, err := env.GetStaticMethodID(jclass, "say", "(Ljava/lang/String;)V")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	env.CallStaticVoidMethod(jclass, jmethodID, arg)
+
 }
